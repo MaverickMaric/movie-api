2d1ea274744df457912787c7c5f89f7be8866e96
Maric Brandi <theoneandonlymandarr@gmail.com>
2020-11-15 03:12:39 -0500

    add GET req for all data

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const movies = require('./movies')
 
 const app = express()
+
+app.get('/', (request, response) => {
+  return response.send(movies)
+})
 
 app.listen(2095, () => {
   console.log('Listening on port 2095...') // eslint-disable-line no-console
