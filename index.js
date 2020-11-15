bd36d6a3658e5786e8fefc2d9dc1310591ad023c
Maric Brandi <theoneandonlymandarr@gmail.com>
2020-11-15 04:07:25 -0500

    add controller layer

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const movies = require('./movies')
+const { getAllMovies } = require('./controller/moviesController')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.send(movies)
+app.get('/movies', getAllMovies)
+
+app.get('/movies/:directors', (request, response) => {
+  const { directors } = request.params
+
+  const foundDir = movies.filter((movie) => movie.directors === directors)
+
+  return response.send(foundDir)
 })
+
+app.get('/movies/:title', (request, response) => {
+  const { title } = request.params
+
+  const foundTitle = movies.filter((movie) => movie.title === title)
+
+  return response.send(foundTitle)
+})
+
 
 app.listen(2095, () => {
   console.log('Listening on port 2095...') // eslint-disable-line no-console
